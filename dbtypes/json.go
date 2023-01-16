505aca69b6b7b060a74e4045034967dae961dcc5
Dr. Abiira Nathan <nabiira2by2@gmail.com>
2023-01-16 15:19:52 +0300

    Add sqlc
    Signed-off-by: Dr. Abiira Nathan <nabiira2by2@gmail.com>

---
+++ dbtypes/json.go
@@ -0,0 +1,22 @@
+package dbtypes
+
+import (
+	"database/sql/driver"
+	"encoding/json"
+)
+
+type JSON map[string]interface{}
+
+// Scan scan value into Jsonb, implements sql.Scanner interface
+func (j *JSON) Scan(value interface{}) error {
+	if err := json.Unmarshal(value.([]byte), &j); err != nil {
+		return err
+	}
+	return nil
+}
+
+// Value return json value, implement driver.Valuer interface
+func (j JSON) Value() (driver.Value, error) {
+	valueString, err := json.Marshal(j)
+	return string(valueString), err
+}
